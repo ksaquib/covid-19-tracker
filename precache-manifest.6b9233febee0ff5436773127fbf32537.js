7c3b1f9692a47b7dbdc46e598af96f44a8792bf2
saquib <saquib@tcs.com>
2020-05-25 12:07:17 +0530

    Updates

---
+++ precache-manifest.6b9233febee0ff5436773127fbf32537.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9c2a01a65efe4b598ecd4d734f853d2",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "3bfcb6859384c64ed9a9",
+    "url": "/covid-19-tracker/static/css/main.92084690.chunk.css"
+  },
+  {
+    "revision": "68cd88a9c9188f4ad2d0",
+    "url": "/covid-19-tracker/static/js/2.036d123e.chunk.js"
+  },
+  {
+    "revision": "98e7206d2472a9ba0c2c6593c9913f5f",
+    "url": "/covid-19-tracker/static/js/2.036d123e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bfcb6859384c64ed9a9",
+    "url": "/covid-19-tracker/static/js/main.81b46253.chunk.js"
+  },
+  {
+    "revision": "2ac71ab25d7f50bf3066",
+    "url": "/covid-19-tracker/static/js/runtime-main.8d8af6b3.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19-tracker/static/media/image.d7265326.png"
+  }
+]);
